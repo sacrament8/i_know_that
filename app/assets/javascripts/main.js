f48690186d1e672b49996a0bb14762b350394bd1
sacrament <xkinked.tailx@gmail.com>
2019-05-31 00:33:06 +0900

    テーブルのスタイリングとlocaleをjaに、time_zoneをTokyoに変更

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,15 +1,19 @@
-// 画面リサイズ時にbodyのheight再調整
-$(window).on("load", function() {
+$(document).on("turbolinks:load", function() {
   // 画面サイズ取得し、bodyheightを画面サイズに
   win_height = $(window).height();
   $("body").css("height", win_height + "px");
   win_height = $(window).height();
   $("html").css("height", win_height + "px");
-});
 
-$(window).resize(function() {
-  win_height = $(window).height();
-  $("body").css("height", win_height + "px");
-  win_height = $(window).height();
-  $("html").css("height", win_height + "px");
+  // flashメッセージ出現、消滅アニメーション
+  $(".delay-vanish")
+    .fadeIn(100)
+    .fadeOut(3600);
+
+  $(window).resize(function() {
+    win_height = $(window).height();
+    $("body").css("height", win_height + "px");
+    win_height = $(window).height();
+    $("html").css("height", win_height + "px");
+  });
 });
