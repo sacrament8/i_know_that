1565921251b491c7dd3f7b9890bdd447e47d0b67
sacrament <xkinked.tailx@gmail.com>
2019-05-30 16:05:08 +0900

    ひそひそ機能実装途中一時保存

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -5,7 +5,7 @@ class RelationsController < ApplicationController
     if user_signed_in?
       @user = User.find(params[:relation][:followed_id])
       current_user.follow!(@user)
-      redirect_to followers_user_path(current_user.id), notice: "#{@user.name}さんをフォローしました"
+      redirect_to followers_user_path(current_user.id), notice: "#{@user.name}さんをひそひそ登録しました"
 
     end  
   end
@@ -13,6 +13,6 @@ class RelationsController < ApplicationController
   def destroy
     @user = Relation.find(params[:id]).followed
     current_user.unfollow!(@user)
-    redirect_to followers_user_path(current_user.id), notice: "#{@user.name}さんへのフォローを解除しました"
+    redirect_to followers_user_path(current_user.id), notice: "#{@user.name}さんとのひそひそを解除しました"
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,20 @@
 module UsersHelper
-  def followers_and_followings(sign_in_user)
-    # followersのid配列取得、followingのid配列を取得
-    # 配列をpushで結合、uniq!で重複削除
-    # 配列のidからUser.find_byでユーザーインスタンスの配列取得
-    # viewでeachで回す
+
+  # 自分がフォローするユーザー、自分をフォローしているユーザーのidを配列で取得
+  def get_followers_and_followings_ids(sign_in_user)
     followings_ids = sign_in_user.following.pluck(:id)
     followers_ids = sign_in_user.followers.pluck(:id)
     followers_and_followings_ids = (followings_ids + followers_ids).uniq
-    @followers_and_followings = User.where(id: followers_and_followings_ids)
   end
+
+  # 自分がフォローするユーザー、自分をフォローしているユーザーのインスタンスを配列で取得
+  def followers_and_followings(sign_in_user)
+    followers_and_followings = User.where(id: get_followers_and_followings_ids(sign_in_user))
+  end
+
+  # 自分が相手を、または相手が自分をフォローしていればtrueをそうでなければfaleを返す
+  def already_linked?(sign_in_user, other_user)
+    get_followers_and_followings_ids(sign_in_user).include?(other_user.id)
+  end
+
 end
