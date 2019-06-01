41132c7e72f3662385cf340c95ad162823a62286
sacrament <xkinked.tailx@gmail.com>
2019-06-02 01:22:01 +0900

    navbarをfixedに

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,6 +4,7 @@ $(document).on("turbolinks:load", function() {
   $("body").css("height", win_height + "px");
   win_height = $(window).height();
   $("html").css("height", win_height + "px");
+  $("document").css("margin-top", "50px");
 
   // flashメッセージ出現、消滅アニメーション
   $(".delay-vanish")
@@ -15,5 +16,24 @@ $(document).on("turbolinks:load", function() {
     $("body").css("height", win_height + "px");
     win_height = $(window).height();
     $("html").css("height", win_height + "px");
+  });
+  $(function() {
+    var $win = $(window),
+      $main = $("main"),
+      $nav = $("nav"),
+      navHeight = $nav.outerHeight(),
+      navPos = $nav.offset().top,
+      fixedClass = "is-fixed";
+
+    $win.on("load scroll", function() {
+      var value = $(this).scrollTop();
+      if (value > navPos) {
+        $nav.addClass(fixedClass);
+        $main.css("margin-top", navHeight);
+      } else {
+        $nav.removeClass(fixedClass);
+        $main.css("margin-top", "0");
+      }
+    });
   });
 });
