f763e761bcf165267d80e86611a8b773c9bf581a
sacrament <xkinked.tailx@gmail.com>
2019-06-07 18:28:06 +0900

    seed生成の修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,12 +30,12 @@ namespace :deploy do
     end
   end
 
-  desc 'Run seed'
-  task :seed do
+  desc 'Load seed data into database'
+  task :seed_fu do
     on roles(:app) do
-      with rails_env:fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, :exec, :rails, 'db:seed_fu'
         end
       end
     end
