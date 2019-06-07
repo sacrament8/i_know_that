c933381ee501c115a8866eb927d59897857e2bff
sacrament <xkinked.tailx@gmail.com>
2019-06-07 20:13:26 +0900

    deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,16 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
+  task :db_reset do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:migrate:reset"
+        end
+      end
+    end
+  end
+
   desc 'Create database'
   task :db_create do
     on roles(:db) do |host|
