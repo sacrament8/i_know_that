0d8ab038fd876b103e974e1fb3304020491f6194
sacrament <xkinked.tailx@gmail.com>
2019-06-08 00:42:02 +0900

    deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,31 @@
-# config valid only for current version of Capistrano
 lock '3.6.0'
 
-set :application, 'i_know_that'
-set :repo_url, 'https://github.com/sacrament8/i_know_that'
+# デプロイするアプリケーション名
+set :application, 'achieve'
 
+# cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
+set :repo_url, 'https://github.com/xxxxxxxxx/yyyyyyyyy'
+
+# deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
 
-set :deploy_to, '/var/www/i_know_that'
+# deploy先のディレクトリ。
+set :deploy_to, '/var/www/achieve'
+
+# シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
-set :keep_releases, 5
-set :rbenv_ruby, '2.5.3'
-set :rbenv_type, :system
-set :log_level, :info
 
+# 保持するバージョンの個数(※後述)
+set :keep_releases, 5
+
+# Rubyのバージョン
+set :rbenv_ruby, '2.5.1'
+set :rbenv_type, :system
+
+#出力するログのレベル。エラーログを詳細に見たい場合は :debug に設定する。
+#本番環境用のものであれば、 :info程度が普通。ただし挙動をしっかり確認したいのであれば :debug に設定する。
+set :log_level, :info
 
 namespace :deploy do
   desc 'Restart application'
@@ -21,31 +33,35 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  task :db_reset do
-    on roles(:app) do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, "db:reset"
-        end
-      end
-    end
-  end
-
   desc 'Create database'
   task :db_create do
     on roles(:db) do |host|
-      with rails_env:fetch(:rails_env) do
+      with rails_env: fetch(:rails_env) do
         within current_path do
           execute :bundle, :exec, :rake, 'db:create'
         end
       end
     end
   end
+
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
   after :publishing, :restart
-  before :restart, :clear_cache do
+
+  after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
     end
   end
+
   before :publishing, 'db:seed_fu'
   desc 'Load seed data into database'
   task :seed_fu do
@@ -58,4 +74,3 @@ namespace :deploy do
     end
   end
 end
-  
