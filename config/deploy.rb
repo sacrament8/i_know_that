10890caa5e95f2fda8233a81d7baab76fef5579a
sacrament <xkinked.tailx@gmail.com>
2019-06-07 20:26:30 +0900

    deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,17 @@ namespace :deploy do
       end
     end
   end
+  task :db_reset do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:migrate:reset"
+        end
+      end
+    end
+  end
   after :publishing, :restart
+
   
 
   after :restart, :clear_cache do
