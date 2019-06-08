8e88136aa99f93fc4d0de577584c635cac0fbd4f
sacrament <xkinked.tailx@gmail.com>
2019-06-08 21:11:48 +0900

    一時保存

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module PortfolioApp
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :utc
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
