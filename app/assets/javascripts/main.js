85d9a3f4453a4a21f0feca8d56939b447f168292
sacrament <xkinked.tailx@gmail.com>
2019-06-08 15:59:43 +0900

    jquery読み込み修正

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -17,23 +17,21 @@ $(document).on("turbolinks:load", function() {
     win_height = $(window).height();
     $("html").css("height", win_height + "px");
   });
-  $(function() {
-    var $win = $(window),
-      $main = $("main"),
-      $nav = $("#nav"),
-      navHeight = $nav.outerHeight(),
-      navPos = $nav.offset().top,
-      fixedClass = "is-fixed";
+  var $win = $(window),
+    $main = $("main"),
+    $nav = $("#nav"),
+    navHeight = $nav.outerHeight(),
+    navPos = $nav.offset().top,
+    fixedClass = "is-fixed";
 
-    $win.on("load scroll", function() {
-      var value = $(this).scrollTop();
-      if (value > navPos) {
-        $nav.addClass(fixedClass);
-        $main.css("margin-top", navHeight);
-      } else {
-        $nav.removeClass(fixedClass);
-        $main.css("margin-top", "0");
-      }
-    });
+  $win.on("load scroll", function() {
+    var value = $(this).scrollTop();
+    if (value > navPos) {
+      $nav.addClass(fixedClass);
+      $main.css("margin-top", navHeight);
+    } else {
+      $nav.removeClass(fixedClass);
+      $main.css("margin-top", "0");
+    }
   });
 });
